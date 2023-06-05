6fee9b6c4fabcd483784b062459149da3bebe7f5
Gripers <gripers91@gmail.com>
2023-06-05 20:46:05 +0500

    test 3.0

--- src/store/api.ts
+++ src/store/api.ts
@@ -8,23 +8,24 @@ import {
   fetchBaseQuery,
 } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API });
-const customBaseQuery: BaseQueryFn<
-  string | FetchArgs,
-  unknown,
-  FetchBaseQueryError
-> = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
+// const baseQuery = fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API });
+// const customBaseQuery: BaseQueryFn<
+//   string | FetchArgs,
+//   unknown,
+//   FetchBaseQueryError
+// > = async (args, api, extraOptions) => {
+//   const result = await baseQuery(args, api, extraOptions);
 
-  return {
-    // @ts-ignore
-    data: result.data?.data,
-    meta: result.meta,
-  };
-};
+//   return {
+//     // @ts-ignore
+//     data: result.data?.data,
+//     meta: result.meta,
+//   };
+// };
 
 export const rootApi = createApi({
   reducerPath: 'root',
-  baseQuery: customBaseQuery,
+  // baseQuery: customBaseQuery,
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API }),
   endpoints: () => ({}),
 });
