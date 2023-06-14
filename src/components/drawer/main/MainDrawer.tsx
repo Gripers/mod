ef81ffaf088d5f20b6b129ef053206eee024ede1
Gripers <gripers91@gmail.com>
2023-06-14 23:08:37 +0500

    drawer added

--- src/components/drawer/main/MainDrawer.tsx
+++ src/components/drawer/main/MainDrawer.tsx
@@ -2,6 +2,8 @@ import { Dispatch, SetStateAction } from "react";
 import styles from "./MainDrawer.module.scss";
 
 import { Drawer, Box } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 
 type MainDrawerProps = {
   open: boolean;
@@ -21,7 +23,54 @@ const MainDrawer = ({ open, setOpen }: MainDrawerProps) => {
       PaperProps={{ sx: { width: "100%" } }}
     >
       <Box sx={boxStyles}>
-        <div className={styles.main__drawer}></div>
+        <div className={styles.main__drawer}>
+          <div className={styles.main__drawer_header}>
+            <div className={styles.main__drawer__header_top}>
+              <button onClick={() => setOpen(false)}>
+                <Image
+                  src="/static/media/close.svg"
+                  alt=""
+                  width={16}
+                  height={16}
+                />
+              </button>
+            </div>
+            <nav className={styles.main__drawer__header_bottom}>
+              <ul>
+                <li>
+                  <Link href="/login">
+                    <Image
+                      src="/static/media/lock.svg"
+                      alt=""
+                      width={16}
+                      height={16}
+                    />
+                    <p>ВХОД</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/registration">
+                    <Image
+                      src="/static/media/pen.svg"
+                      alt=""
+                      width={16}
+                      height={16}
+                    />
+                    <p>РЕГИСТРАЦИЯ</p>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className={styles.main__drawer_body}>
+            <nav className={styles.main__drawer__body_top}>
+              <ul>
+                <li>ЖЕНЩИНАМ</li>
+                <li>МУЖЧИНАМ</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </Box>
     </Drawer>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from 'react';
-import '@/styles/globals.scss';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+import { useEffect, useState } from "react";
+import "@/styles/globals.scss";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
-import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import NextNProgress from 'nextjs-progressbar';
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import NextNProgress from "nextjs-progressbar";
 
-import { DrawerContext } from '@/context/DrawerContext';
-import { persistor, store } from '@/store/store';
-import { ModalContext } from '@/context/ModalContext';
-import Layout from '@/components/layout/layout';
+import { DrawerContext } from "@/context/DrawerContext";
+import { persistor, store } from "@/store/store";
+import { ModalContext } from "@/context/ModalContext";
+import Layout from "@/components/layout/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [open, setOpen] = useState(false);
   const [confirmOpen, setConfirmOpen] = useState(false);
 
   useEffect(() => {
-    const loader = document.getElementById('global__loader');
+    const loader = document.getElementById("global__loader");
     if (loader)
       setTimeout(() => {
-        loader.style.display = 'none';
-        document.body.removeAttribute('id');
+        loader.style.display = "none";
+        document.body.removeAttribute("id");
       }, 2000);
   }, []);
 
@@ -33,7 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <ModalContext.Provider value={{ confirmOpen, setConfirmOpen }}>
             <Layout>
               <NextNProgress
-                color='black'
+                color="black"
                 height={2}
                 options={{ showSpinner: false }}
               />
